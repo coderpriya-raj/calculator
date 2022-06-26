5df3c7c7fa3e8eeffecfb2ec37b1ceeb45b85c2a
coderpriya-raj <108212793+coderpriya-raj@users.noreply.github.com>
2022-06-26 20:01:14 +0530

    Add files via upload

---
+++ calculator1.py
@@ -0,0 +1,20 @@
+num1=float(input("Enter the first number\n"))
+num2=float(input("Enter the second number\n"))
+op=input("enter operation you want\n")
+
+if op=="+":
+    print(num1+num2)
+elif op=="-":
+    print(num1-num2)
+elif op=="*":
+    print(num1*num2)
+elif op=="/":
+    print(num1/num2)        
+elif op=="**":
+    print(num1**num2)
+elif op=="%":
+    print(num1%num2)  
+elif op=="**":
+    print(num1**num2)         
+else:
+    print("invalid input")   
